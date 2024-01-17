526c95dcbefea61d7d96d7d09e5ba0e01f357c26
JKL <james.lindley@superdry.com>
2024-01-17 17:31:21 +0000

    added shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
-import './globals.css';
+import { cn } from '@/lib/utils';
+import '@/styles/global.css';
+import { Inter as FontSans } from 'next/font/google';
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>{children}</body>
     </html>
   );
 }
