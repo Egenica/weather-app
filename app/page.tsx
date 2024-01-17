b13354ff3e1dfa89a25111aa6fde448408bd7328
JKL <james.lindley@superdry.com>
2024-01-17 15:53:49 +0000

    Weather app init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export const metadata = {
-  title: "App Router",
+  title: 'App Router',
 };
 
+import Counter from './counter';
+
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <>
+      <h1>App Router wd</h1>
+      <Counter />
+    </>
+  );
 }
