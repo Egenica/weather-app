8c48f2e9893c63f1c973c717ce49cc925d33d8ba
JKL <16577036+Egenica@users.noreply.github.com>
2025-01-15 21:36:04 +0000

    updated readme and cleaned up

--- README.md
+++ README.md
@@ -1,35 +1,27 @@
-# Next.js + Jest
+# Weather App - Using Met Office API (W.I.P)
 
-This example shows how to configure Jest to work with Next.js.
+API Link
 
-This includes Next.js' built-in support for Global CSS, CSS Modules and TypeScript. This example also shows how to use Jest with the App Router and React Server Components.
+- 'https://www.metoffice.gov.uk/services/data/datapoint'
 
-> **Note:** Since tests can be co-located alongside other files inside the App Router, we have placed those tests in `app/` to demonstrate this behavior (which is different than `pages/`). You can still place all tests in `__tests__` if you prefer.
+Run Dev
 
-## Deploy your own
+`npm run dev`
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-jest&project-name=with-jest&repository-name=with-jest)
+Run Production
 
-## How to Use
+`npm run build`
 
-Quickly get started using [Create Next App](https://github.com/vercel/next.js/tree/canary/packages/create-next-app#readme)!
+`npm run start`
 
-In your terminal, run the following command:
+Run Test
 
-```bash
-npx create-next-app --example with-jest with-jest-app
-```
+`npm run test`
 
-```bash
-yarn create next-app --example with-jest with-jest-app
-```
+# ToDo
 
-```bash
-pnpm create next-app --example with-jest with-jest-app
-```
-
-## Running Tests
-
-```bash
-npm test
-```
+- Tests need updating / adding to some sections
+- Currently the 'Now at a glance' uses the first row of data so its not time accurate (unless it's the morning - this was added as a nice to have and needs to be time accurate)
+- Uses two api calls but might needs to change to one as it's potentially overyly complicated as merges the two to display the time
+- More backgrounds need adding for the counties currently it displays various pictures of yorkshire / lancs based on the quick view weather
+- Some wind direction icons might not be showing
--- components/server/weather.location.server.ts
+++ components/server/weather.location.server.ts
@@ -46,17 +46,6 @@ type WeatherLocationDataProps = {
   id: string | null;
 };
 
-// type TimeStampsDataProps = {
-//   Resource: {
-//     dataData: string;
-//     res: string;
-//     type: string;
-//     TimeSteps: {
-//       TS: string[];
-//     };
-//   };
-// };
-
 export type TimeStampsDataProps = {
   Resource: {
     TimeSteps: {
@@ -68,37 +57,7 @@ export type TimeStampsDataProps = {
   };
 };
 
-// export async function getWeatherLocationData({ id }: WeatherLocationDataProps) {
-//   // Try to get the data from session storage
-//   // const storedData = sessionStorage.getItem('location');
-//   const storedData = null;
-
-//   if (storedData) {
-//     // If the data is in session storage, parse it and return it
-//     // console.log(JSON.stringify(JSON.parse(storedData).splice(0, 10)));
-//     return JSON.parse(storedData) as Location[];
-//   } else {
-//     // If the data is not in session storage, fetch it
-//     const res = await fetch(
-//       `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${id}?res=3hourly&key=5df7f8b3-a40e-4294-8e05-ce08618aca19`,
-//     );
-
-//     const data: Location = await res.json();
-
-//     // Store the fetched data in session storage
-//     sessionStorage.setItem('location', JSON.stringify(data));
-
-//     // console.log(data.Locations.Location);
-
-//     return data;
-//   }
-// }
-
 export async function getWeatherLocationData({ id }: WeatherLocationDataProps) {
-  // Try to get the data from session storage
-  // const storedData = sessionStorage.getItem('location');
-  // const storedData = null;
-
   // If the data is not in session storage, fetch it
   const res = await fetch(
     `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${id}?res=3hourly&key=5df7f8b3-a40e-4294-8e05-ce08618aca19`,
@@ -109,33 +68,8 @@ export async function getWeatherLocationData({ id }: WeatherLocationDataProps) {
 
   const data: Location = await res.json();
 
-  //console.log(data);
-
   return data;
 }
-
-// export async function getCurrentLocationData({ id }: WeatherLocationDataProps) {
-//   // Try to get the data from session storage
-//   // const storedData = sessionStorage.getItem('location');
-//   // const storedData = null;
-
-//   console.log('id', id);
-
-//   // If the data is not in session storage, fetch it
-
-//   const res = await fetch(
-//     `http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/${id}?res=hourly&key=5df7f8b3-a40e-4294-8e05-ce08618aca19`,
-//     {
-//       cache: 'no-store',
-//     },
-//   );
-
-//   const data: Location = await res.json();
-
-//   //console.log(data);
-
-//   return data;
-// }
 
 export async function getTimeStampsData() {
   const res = await fetch(
@@ -146,8 +80,6 @@ export async function getTimeStampsData() {
   );
 
   const data: TimeStampsDataProps = await res.json();
-
-  //console.log(data);
 
   return data;
 }
