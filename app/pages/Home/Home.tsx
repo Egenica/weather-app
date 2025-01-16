ac27a9d379aa9f130b084481ecbcf90027c411f0
JKL <16577036+Egenica@users.noreply.github.com>
2025-01-16 16:49:52 +0000

    refactor

--- app/pages/Home/Home.tsx
+++ app/pages/Home/Home.tsx
@@ -5,18 +5,8 @@ import { WeatherLocation } from '@/components/WeatherLocation/WeatherLocation';
 import WeatherSearch from '@/components/WeatherSearch/WeatherSearch';
 import { WeatherLocationT } from '@/components/server/weather.server';
 import { useEffect, useState } from 'react';
-import PullToRefresh from 'react-pull-to-refresh';
 
 export default function Home() {
-  const handleRefresh = () => {
-    return new Promise<void>((resolve) => {
-      setTimeout(() => {
-        window.location.reload(); // Refresh the page
-        resolve();
-      }, 500);
-    });
-  };
-
   const [location, setLocation] = useState<WeatherLocationT | null>(null);
 
   useEffect(() => {
@@ -28,23 +18,21 @@ export default function Home() {
   }, []);
 
   return (
-    <PullToRefresh onRefresh={handleRefresh} style={{ height: '100vh', overflow: 'auto' }}>
-      <div>
-        <MiniNav data-testid="mini-nav" location={location} setLocation={setLocation} />
-        <div className="container">
-          {!location ? (
-            <>
-              <h1 className="mb-10 mt-10 text-center text-4xl font-thin text-teal-600">Find your weather location</h1>
-              <WeatherSearch data-testid="weather-search" setLocation={setLocation} />
-            </>
-          ) : (
-            <>
-              <h1 className="mb-10 mt-10 text-center text-4xl font-thin text-teal-600">{location.name}</h1>
-              <WeatherLocation id={location.id} />
-            </>
-          )}
-        </div>
+    <div>
+      <MiniNav data-testid="mini-nav" location={location} setLocation={setLocation} />
+      <div className="container">
+        {!location ? (
+          <>
+            <h1 className="mb-10 mt-10 text-center text-4xl font-thin text-teal-600">Find your weather location</h1>
+            <WeatherSearch data-testid="weather-search" setLocation={setLocation} />
+          </>
+        ) : (
+          <>
+            <h1 className="mb-10 mt-10 text-center text-4xl font-thin text-teal-600">{location.name}</h1>
+            <WeatherLocation id={location.id} />
+          </>
+        )}
       </div>
-    </PullToRefresh>
+    </div>
   );
 }
--- components/BodyComp/BodyComp.tsx
+++ components/BodyComp/BodyComp.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from '@/lib/utils';
 import { Inter as FontSans } from 'next/font/google';
+import PullToRefresh from 'react-pull-to-refresh';
 
 import { weatherType } from '../TodaysWeather/weatherType';
 
@@ -9,24 +10,35 @@ const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 });
+
 export default function BodyComp({ children }: { children: React.ReactNode }) {
+  const handleRefresh = () => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        window.location.reload(); // Refresh the page
+        resolve();
+      }, 500);
+    });
+  };
   const weatherNow = JSON.parse(localStorage.getItem('weatherNow') as string);
 
   // console.log(weatherNow);
 
   return (
-    <div
-      className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
-      style={{
-        background: weatherNow
-          ? `url(${weatherType(weatherNow.W)[2]})`
-          : 'url(/_fcf1d22e-7641-4978-ba2a-02aa3218c2ab.jpeg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent" />
-      <div className="relative z-10">{children}</div>
-    </div>
+    <PullToRefresh onRefresh={handleRefresh} style={{ height: '100vh', overflow: 'auto' }}>
+      <div
+        className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
+        style={{
+          background: weatherNow
+            ? `url(${weatherType(weatherNow.W)[2]})`
+            : 'url(/_fcf1d22e-7641-4978-ba2a-02aa3218c2ab.jpeg)',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent" />
+        <div className="relative z-10">{children}</div>
+      </div>
+    </PullToRefresh>
   );
 }
