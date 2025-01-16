e41496ace0e47c4f257419b31f3e0cc37da2808d
JKL <16577036+Egenica@users.noreply.github.com>
2025-01-16 00:24:09 +0000

    added pwa functions and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,11 @@ const BodyComp = dynamic(() => import('@/components/BodyComp/BodyComp'), {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/iOs-Icon.png" />
+      </head>
       <body>
         <BodyComp>{children}</BodyComp>
       </body>
---
+++ next.config.cjs
@@ -0,0 +1,6 @@
+import withPWA from 'next-pwa';
+
+module.exports = withPWA({
+  dest: 'public',
+  reactStrictMode: false,
+});
