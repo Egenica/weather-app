ccef1660bf0663ae6d2cdd901b375df9f3d3aae6
JKL <james.lindley@superdry.com>
2026-02-18 21:57:56 +0000

    fix: update page metadata for improved clarity and accuracy in weather information

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Home from './pages/Home/Home';
 
-// import { MiniNav } from '@/components/MiniNav/MiniNav';
-// import WeatherSearch from '@/components/WeatherSearch/WeatherSearch';
-
 export const metadata = {
-  title: '5 Day forcast - Weather App',
-  description: 'Dynamic Weather App',
+  title: 'weathergrid.co.uk - 5 Day UK weather forcast',
+  description:
+    'Dynamic Weather App, providing accurate and up-to-date weather forecasts for the UK. Get real-time weather updates, hourly forecasts, and 5-day predictions for your location. Stay prepared with our user-friendly interface and reliable weather information.',
 };
 
 export default function Page() {
